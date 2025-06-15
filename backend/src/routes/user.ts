4f056781b79d7f3161ff2e48921affc5e2f16108
ronak <ronakmaheshwari077@gmail.com>
2025-06-16 01:32:39 +0530

    Added user backend

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,7 +2,86 @@ import express from "express"
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
 import dotenv from "dotenv"
+import prisma from "../lib/prisma/prisma"
+import { SigninSchema, SignupSchema } from "../types"
 
 const userRouter = express.Router()
+dotenv.config();
+const saltrounds = Number(process.env.Saltrounds) || 10
+const jwtsecret = process.env.JWTSecret || "123456"
+
+userRouter.post('/signup',async(req:any,res:any)=>{
+    try {
+        const { success, data } = SignupSchema.safeParse(req.body);
+        if(!success){
+            return res.status(400).json({
+                message:"Invalid inputs were provided"
+            })
+        }
+        const { username, email, password } = data;
+        const ExistingUser = await prisma.user.findUnique({
+            where:{
+                email:email
+            }
+        })
+        if(ExistingUser){
+            return res.status(400).json({
+                message:"Email already exists in the Database"
+            })
+        }
+        const hash = await bcrypt.hash(password, saltrounds);
+        const newUser = await prisma.user.create({
+            data:{
+                username,email,password:hash
+            }
+        })
+        const token = jwt.sign({userId: newUser.id},jwtsecret)
+        return res.status(200).json({
+            message:"User Successfully Added",
+            token:token
+        })
+    } catch (error) {
+         return res.status(500).json({
+            message:"Internal Server Error"
+        })
+    }
+})
+
+userRouter.post('/signin',async(req:any,res:any)=>{
+    try {
+        const {success, data} = SigninSchema.safeParse(req.body);
+        if(!success){
+             return res.status(400).json({
+                message:"Invalid inputs were provided"
+            })
+        }
+        const {email,password} = data;
+        const ExistingUser = await prisma.user.findUnique({
+            where:{
+                email
+            }
+        })
+        if(!ExistingUser){
+            return res.status(404).json({
+                message:"User doesn't exist"
+            })
+        }
+        const hash = await bcrypt.compare(ExistingUser.password,password);
+        if(!hash){
+            return res.status(404).json({
+                message:"Invalid Password recieved"
+            })
+        }
+        const token = jwt.sign({userId: ExistingUser.id},jwtsecret);
+        return res.status(200).json({
+            message:"Logged in successfully",
+            token:token
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message:"Internal Server Error"
+        })
+    }
+})
 
 export default userRouter
--- backend/src/types.ts
+++ backend/src/types.ts
@@ -1,1 +1,36 @@
 import z from "zod";
+
+export const SignupSchema = z.object({
+    username:z.string().min(4).max(20),
+    email:z.string().email(),
+    password:z.string().min(8).regex(
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()\-_=+{};:,<.>]).{8,}$/, 
+  "Password must be at least 8 characters long and include uppercase, lowercase, number, and special character.")
+})
+
+export const SigninSchema = z.object({
+  email:z.string().email(),
+  password:z.string().min(8).regex(
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()\-_=+{};:,<.>]).{8,}$/, 
+  "Password must be at least 8 characters long and include uppercase, lowercase, number, and special character.")
+})
+
+export const acceptedMimeTypes = [
+  'image/jpeg',
+  'image/png',
+  'image/webp',
+  'image/*',
+  'video/mp4',
+  'video/quicktime'
+]
+
+export const fileZod = z.object({
+  originalname:z.string(),
+  mimetype: z.string().refine(t => acceptedMimeTypes.includes(t), {
+    message: 'Unsupported file type'
+  }),
+  size: z.number().max(10 * 1024 * 1024 * 1024,{ message: 'Max 100 MB allowed' }),
+  buffer: z.instanceof(Buffer)
+})
+
+export const filesZod = z.array(fileZod)
