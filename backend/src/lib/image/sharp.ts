3ec68d0e9e381b631c86809ca7c7068da3cbd662
ronak <ronakmaheshwari077@gmail.com>
2025-06-17 19:46:53 +0530

    Backend Speed Increase

--- backend/src/lib/image/sharp.ts
+++ backend/src/lib/image/sharp.ts
@@ -1,10 +1,11 @@
 import sharp from "sharp";
 
-export default async function processImage(buffer:Buffer):Promise<Buffer> {
-    return await sharp(buffer).webp({
-        quality:100,
-        effort:6,
-        lossless:true
+export default async function processImage(buffer: Buffer): Promise<Buffer> {
+  return await sharp(buffer)
+    .webp({
+      quality: 85,       
+      effort: 1,         
+      lossless: false   
     })
-    .toBuffer()
+    .toBuffer();
 }
